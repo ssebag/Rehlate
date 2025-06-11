7cedc93d3514ee0d1da6c0de8ff43181e9dd3a4d
unknown <s.sseba96@gmail.com>
2025-06-10 23:51:37 -0700

    add swiper

--- src/Component/Rehlate/Rehlate.jsx
+++ src/Component/Rehlate/Rehlate.jsx
@@ -7,12 +7,9 @@ import { useTranslation } from 'react-i18next';
 export default function Rehlate() {
     const { t, i18n } = useTranslation();
   return (
-    <div className="rehlate">
-     <div className={`${i18n.language ==='ar' ? 'arabic' : ''} `}>  
+    <div className={`rehlate ${i18n.language ==='ar' ? 'arabic' : ''} `}>
         <Title  titleParagraph= {t('mainTitle.Rehlate.Paraghraph')} titleMain={t('mainTitle.Rehlate.MainTitle')} />
-      </div>     
         <SwiperImg />
-     
     </div>
   )
 }
